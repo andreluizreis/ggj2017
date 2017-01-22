60ccab82578e100c4be667226216828677647c71
Andre Reis <andreluizreis@gmail.com>
2017-01-21 22:57:34 -0500

    Player 1 animation

---
+++ GGJ2017/Assets/Scripts/EnergyBar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyBar : MonoBehaviour {
+
+    public Sprite[] energyImages;
+    public int maxEnergy = 9; 
+    public int currentEnergy = 5; 
+
+
+	// Use this for initialization
+	void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (currentEnergy < 10 && currentEnergy >= 0)
+        {
+            GetComponent<SpriteRenderer>().sprite = energyImages[currentEnergy];
+        }
+	}
+}
---
+++ GGJ2017/Assets/Scripts/Hearts.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hearts : MonoBehaviour {
+    	
+	public Sprite[] heartImages;
+    public int currentHeart = 3; 
+
+
+    void Start(){
+        
+	}
+	
+
+	void Update() {
+        if (currentHeart < 7 && currentHeart >= 0)
+        {
+            GetComponent<SpriteRenderer>().sprite = heartImages[currentHeart];
+        }
+	}
+
+}
